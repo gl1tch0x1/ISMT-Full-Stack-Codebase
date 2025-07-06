60193394156d7cec1cf4175b70021af536585592
gl1tch0x1 <pandeyaashish48@gmail.com>
2025-07-06 09:31:54 +0545

    commit 0.1

---
+++ day01.js
@@ -0,0 +1,22 @@
+// 1. let  (example):
+    let y = 5;
+    // let y = 15; // Error: Cannot re-declare block-scoped variable 'y'
+    y = 25;      // Update is allowed
+    console.log(y) // printing the output
+
+// 2. var (example):
+    var x = 10;
+    var x = 20; // Re-declaration is allowed
+    x = 30;     // Update is allowed
+    console.log(x)
+
+// 3. const (example):
+    const PI = 3.14;
+    // PI = 3.14159; // Error: Assignment to constant variable
+    // const PI = 3.14159; // Error: Cannot re-declare block-scoped variable 'PI'
+
+    const person = { name: "Alice" };
+    person.name = "Bob"; // Allowed: modifying object content
+    // person = { name: "Charlie" }; // Error: Assignment to constant variable
+    console.log(person)
+    
