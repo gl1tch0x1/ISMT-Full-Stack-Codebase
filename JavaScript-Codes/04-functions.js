2ecb45a6dbcece7f11da3b045a5cd42c7a7f6634
gl1tch0x1 <pandeyaashish48@gmail.com>
2025-07-23 10:34:03 +0545

    function-questions-updated

--- JavaScript-Codes/04-functions.js
+++ JavaScript-Codes/04-functions.js
@@ -52,3 +52,44 @@ function printTable(n) {
 }
 
 printTable(num);
+
+
+// Q6. WAP to check if the number is prime or not. (function)
+
+let number01 = prompt("Enter a number: ");
+function isPrime(n) {
+  if (n < 2) {
+    alert(n + " is not a prime number.");
+    return;
+  }
+
+  let isPrime = true;
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  if (isPrime) {
+    alert(n + " is a prime number.");
+  } else {
+    alert(n + " is not a prime number.");
+  }
+}
+isPrime(Number(number01));
+
+
+
+
+// Q7. WAP to if the user is authenticated person or not.(function) [authenticatedPerson = "Ram", "Shyam", "Suresh", "Rupesh".]
+
+let user = prompt("Enter your name:");
+function checkUser(name) {
+    if (name === "Ram" || name === "Shyam" || name === "Suresh" || name === "Rupesh") {
+        console.log("Welcome, " + name + " you are authenticated.");
+    } else {
+        console.log("Sorry, " + name + " is NOT an authenticated.");
+    }
+}
+checkUser(user);
