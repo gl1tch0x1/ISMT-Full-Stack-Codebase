3fadc1221e7e1a4013eb983a6a7a479a6dd7e57c
gl1tch0x1 <pandeyaashish48@gmail.com>
2025-08-11 11:38:19 +0545

    projects & codes updated.

--- JavaScript-Codes/DOM-and-BOM.js
+++ JavaScript-Codes/DOM-and-BOM.js
@@ -18,6 +18,7 @@ Ex: window.location.href = "https://www.example.com";
 */
 
 // Example of DOM manipulation
+/*
 document.addEventListener("DOMContentLoaded", function() {
     const newElement = document.createElement("p");
     newElement.textContent = "This is a new paragraph added to the DOM.";
@@ -31,3 +32,86 @@ window.addEventListener("load", function() {
     alert("Welcome to the page!");
 }
 );
+*/
+
+// Introduction to DOM manipulation with examples,
+
+/*
+
+Que.No.:01 What is a DOM Event in JavaScript?
+
+DOM Event in JavaScript is an action or occurrence that happens in the browser which the browser recognizes 
+and can respond to — such as a user clicking a button, typing in a field, or hovering over an element.
+You can attach event handlers using:
+
+1. HTML attributes (not recommended)
+2. JavaScript DOM methods (recommended)
+
+Simple example of a DOM event:
+*/
+
+
+// 1. onclick – Click on an element
+
+//JS code:
+document.getElementById("myButton").onclick = function () {
+    alert("Button clicked!");
+};
+
+// HTML code:
+<button onclick="alert('Button clicked!')">Click Me</button>
+
+
+// 2. ondblclick – Double-click on an element
+
+// JS code:
+document.getElementById("myButton").ondblclick = function () {
+    alert("Button double-clicked!");
+};
+
+// HTML code:
+<button ondblclick="alert('Button double-clicked!')">Double Click Me</button>
+
+
+// 3. onmouseover – Mouse pointer moves over an element
+
+// JS code:
+document.getElementById("myDiv").onmouseover = function () {
+    this.style.backgroundColor = "yellow";
+}
+
+// HTML code:
+<p onmouseout="this.style.color='black'" style="color:blue;">Move your mouse away</p>
+
+
+// 4. onmouseout – Mouse pointer moves out of an element
+
+// JS code:
+document.getElementById("myDiv").onmouseout = function () {
+    this.style.backgroundColor = "white";
+}
+
+// HTML code:
+<p onmouseout="this.style.color='black'" style="color:blue;">Move your mouse away</p>
+
+
+// 5. onkeydown – Key pressed down
+
+// JS code:
+document.getElementById("myInput").onkeydown = function (event) {
+    console.log("Key pressed: " + event.key);
+};
+
+// HTML code:
+<input type="text" id="myInput" placeholder="Type something..."/>
+
+
+// 6. onkeyup – Key is released
+
+// JS code:
+document.getElementById("myInput").onkeyup = function (event) {
+    console.log("Key released: " + event.key);
+}
+
+// HTML code:
+<input type="text" id="myInput" placeholder="Type something..."/>
---
+++ projects/script.js
@@ -0,0 +1,7 @@
+
+  function showPopup() {
+    document.getElementById("thankYouPopup").style.display = "block";
+    setTimeout(function(){
+      document.getElementById("thankYouPopup").style.display = "none";
+    }, 2000);
+  }
